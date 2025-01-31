92165042b507fdc69663fff9cd6f2c47099126cd
MD RIYAD KHAN <you@example.com>
2025-01-31 16:29:48 +0600

    finally complete stripe checkout with custom link button

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,34 @@
-import { useEffect, useState } from "react";
 import "./App.css";
 
-const ProductDisplay = () => (
-  <section>
-    <div className="product">
-      <img
-        src="https://i.imgur.com/EHyR2nP.png"
-        alt="The cover of Stubborn Attachments"
-      />
-      <div className="description">
-        <h3>Stubborn Attachments</h3>
-        <h5>$20.00</h5>
-      </div>
-    </div>
-    <form action="/create-checkout-session" method="POST">
-      <button type="submit">Checkout</button>
-    </form>
-  </section>
-);
-
 export default function App() {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    // Check to see if this is a redirect back from Checkout
-    const query = new URLSearchParams(window.location.search);
-
-    if (query.get("success")) {
-      setMessage("Order placed! You will receive an email confirmation.");
+  const handleCheckout = async () => {
+    const response = await fetch("http://localhost:5000/create-checkout-session", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    });
+  
+    const data = await response.json();
+    if (data.url) {
+      window.location.href = data.url;
     }
-
-    if (query.get("canceled")) {
-      setMessage(
-        "Order canceled -- continue to shop around and checkout when you're ready."
-      );
-    }
-  }, []);
+  };
 
   return (
     <>
-      {message ? (
-        <section>
-          <p>{message}</p>
-        </section>
-      ) : (
-        <ProductDisplay />
-      )}
+      <section>
+        <div className="product">
+          <img
+            src="https://i.imgur.com/EHyR2nP.png"
+            alt="The cover of Stubborn Attachments"
+          />
+          <div className="description">
+            <h3>Stubborn Attachments</h3>
+            <h5>$20.00</h5>
+          </div>
+        </div>
+          <button onClick={handleCheckout} type="submit">Checkout</button>
+      </section>
+     
     </>
   );
 }
